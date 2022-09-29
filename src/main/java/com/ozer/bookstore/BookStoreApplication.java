0be7fde6e37533bac3d2a5d633c68d4377f22696
OzerBey <yasinozeriletisim@gmail.com>
2022-09-29 23:04:44 +0300

    Update SwaggerUI

--- src/main/java/com/ozer/bookstore/BookStoreApplication.java
+++ src/main/java/com/ozer/bookstore/BookStoreApplication.java
@@ -2,6 +2,7 @@ package com.ozer.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //@SpringBootApplication(exclude = {
 //        org.springframework.boot.autoconfigure.security.SecurityProperties.class,
@@ -13,11 +14,4 @@ public class BookStoreApplication {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
-//    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.ozer.bookstore"))
-//                .build();
-//    }
 }
