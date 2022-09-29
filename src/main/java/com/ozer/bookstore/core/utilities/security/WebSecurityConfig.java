2d635dde8b3a4c1d1f39cf8f492db2c632b24129
OzerBey <yasinozeriletisim@gmail.com>
2022-09-29 23:05:06 +0300

    Update Securit Configuration

--- src/main/java/com/ozer/bookstore/core/utilities/security/WebSecurityConfig.java
+++ src/main/java/com/ozer/bookstore/core/utilities/security/WebSecurityConfig.java
@@ -1,54 +1,31 @@
 package com.ozer.bookstore.core.utilities.security;
 
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        //Now, we can write security config settings here
-//        http.authorizeRequests().anyRequest().authenticated().and()
-//                .httpBasic(); //http basic allowed us here
-//    }
-
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//        authenticationManagerBuilder.inMemoryAuthentication().withUser("ozerbey").password("{noop} @pa55word").roles("USER");
-//    }
-
-//    @Autowired
-//    public void configureGlobalEncrypted(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//        authenticationManagerBuilder.inMemoryAuthentication().withUser("ozerbey").password(passwordEncoder.encode("@pa55word")).roles("USER");
-//    }
-//}
-
-
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").and().withUser("ozerBey").password(passwordEncoder().encode("pa55word@")).roles("USER");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().and()
-                .csrf().disable().authorizeRequests()
-                .antMatchers("/users").hasRole("manager")
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+        http.authorizeRequests().antMatchers("/add").hasAnyRole("USER", "ADMIN").antMatchers("/getAll").permitAll().anyRequest().fullyAuthenticated().and().cors().and().csrf().disable().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // her istekte yeni bir doğrulama isteyerek yeni session açılıp kontrol edilir.
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
